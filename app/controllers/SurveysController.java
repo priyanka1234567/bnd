7a45ef42535eeb31153330c0b4935c54b89db624
priyanka1234567 <priyankadvkr12@gmail.com>
2017-03-10 10:32:38 -0500

    fhfv

--- app/controllers/SurveysController.java
+++ app/controllers/SurveysController.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import models.Surveys;
 
+import models.Users;
 import play.Logger;
 import play.libs.Json;
 import play.mvc.Result;
@@ -15,9 +16,7 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 import static play.mvc.Controller.request;
-import static play.mvc.Results.badRequest;
-import static play.mvc.Results.notFound;
-import static play.mvc.Results.ok;
+import static play.mvc.Results.*;
 
 /**
  * Created by welcome on 1/4/2017.
@@ -77,59 +76,98 @@ public class SurveysController {
 
 
 @Transactional
-    public Result deleteSurveyById(Integer id){
-         Surveys s= jpaApi.em().find(Surveys.class,id);
-         jpaApi.em().remove(s);
+    public Result deleteSurveyById(Integer id1){
+    String token = request().getHeader("Authentication");
+    String uname = request().getHeader("id");
+    Result auth = auth(token, uname);
+    Logger.debug(token);
+    Logger.debug(uname);
+    Logger.debug(auth.toString());
+    if(200 == auth.status()) {
+        Surveys s = jpaApi.em().find(Surveys.class, id1);
+        jpaApi.em().remove(s);
 
-         if(null == s){
-             return badRequest("entry not available");
-         }
-         return ok("survey deleted");
+        if (null == s) {
+            return badRequest("entry not available");
+        }
+        return ok("survey deleted");
+    }
+    return unauthorized("unauthorized user");
     }
 
     @Transactional
     public Result addNewSurvey(){
-        final JsonNode json = request().body().asJson();
-        if (null == json) {
-            Logger.error("Unable to get json from request");
-            return badRequest("Unable to get json from request");
-        }
+        String token = request().getHeader("Authentication");
+        String uname = request().getHeader("id");
+        Result auth = auth(token, uname);
+        Logger.debug(token);
+        Logger.debug(uname);
+        Logger.debug(auth.toString());
+        if(200 == auth.status()) {
+            final JsonNode json = request().body().asJson();
+            if (null == json) {
+                Logger.error("Unable to get json from request");
+                return badRequest("Unable to get json from request");
+            }
 
-        final Surveys s = Json.fromJson(json, Surveys.class);
-        if (null == s) {
-            Logger.error("Unable to parse json to Book object");
-            return badRequest("Unable to parse json to Surveys object");
-        }
+            final Surveys s = Json.fromJson(json, Surveys.class);
+            if (null == s) {
+                Logger.error("Unable to parse json to Book object");
+                return badRequest("Unable to parse json to Surveys object");
+            }
 
-        jpaApi.em().persist(s);
-        return ok(json);
+            jpaApi.em().persist(s);
+            return ok(json);
+        }
+        return unauthorized("unauthorized user");
+
     }
+    public Result auth(String token, String id) {
+        final Users users = jpaApi.em().find(Users.class, id);
+        if(null != users) {
+            JsonNode json = Json.toJson(users);
+            if (token.equals(json.path("token").asText())) {
+                return ok();
+            }
+        }
+        return badRequest();
 
+    }
 
     @Transactional
     public Result updateSurvey(Integer id) {
-        final JsonNode json = request().body().asJson();
-        if (null == json) {
+        String token = request().getHeader("Authentication");
+        String uname = request().getHeader("id");
+        Result auth = auth(token, uname);
+        Logger.debug(token);
+        Logger.debug(uname);
+        Logger.debug(auth.toString());
+        if (200 == auth.status()) {
+            final JsonNode json = request().body().asJson();
+            if (null == json) {
 
-            return badRequest("json not found");
+                return badRequest("json not found");
+            }
+
+            if (null == id) {
+                return badRequest("id not found");
+            }
+
+
+            Surveys s = Json.fromJson(json, Surveys.class);
+            if (null == s) {
+                return badRequest("not found");
+            }
+            Surveys old = jpaApi.em().find(Surveys.class, id);
+            old.setSname(s.getSname());
+            old.setSdescription(s.getSdescription());
+            old.setDate(s.getDate());
+            jpaApi.em().merge(old);
+            return ok("updated Successfully ");
         }
-
-        if(null == id){
-            return  badRequest("id not found");
-        }
-
-
-        Surveys s=Json.fromJson(json,Surveys.class);
-        if(null==s)
-        {
-            return badRequest("not found");
-        }
-        Surveys old=jpaApi.em().find(Surveys.class,id);
-        old.setSname(s.getSname());
-        old.setSdescription(s.getSdescription());
-        old.setDate(s.getDate());
-        jpaApi.em().merge(old);
-        return ok("updated Successfully ");
+        return unauthorized("unauthorized user");
     }
+
+
 }
 
